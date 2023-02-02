13eda9971de1085b7935f11f05faf1541ce4bc1b
MattBanuag <matthewbanuag.dev@gmail.com>
2023-02-02 14:15:50 -0600

    build: menu method

--- Game.cs
+++ Game.cs
@@ -12,6 +12,7 @@ namespace RPGGame
         #region Methods
         public static void PlayIntroduction()
         {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine("Welcome to HavenBorne!");
         }
         public static Hero GetHeroName()
@@ -22,10 +23,13 @@ namespace RPGGame
             {
                 try
                 {
+                    Console.ForegroundColor = ConsoleColor.White;
                     Console.Write("Are you the promised hero they speak of? What do they call you? ");
+                    Console.ForegroundColor = ConsoleColor.Cyan;
                     string heroName = Console.ReadLine();
                     newHero = new Hero(heroName);
 
+                    Console.ForegroundColor = ConsoleColor.White;
                     Console.WriteLine($"\nI see.... Greetings {heroName}!");
                     isValid = true;
                 }
@@ -39,11 +43,40 @@ namespace RPGGame
 
             return newHero;
         }
+        public static void ShowMainMenu()
+        {
+            Console.WriteLine("\nPrepare yourself for the fight ahead or explore around. Up to you.");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Select an option: ");
+            Console.WriteLine("1 - Stats");
+            Console.WriteLine("2 - Inventory");
+            Console.WriteLine("3 - Fight!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("\nSo what'll it be? ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            int userChoice = 0;
+            bool isValid = int.TryParse(Console.ReadLine(), out userChoice);
+
+            while (!isValid)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.Write("I apologize, but this option does not exist.");
+                Console.ResetColor();
+                Console.WriteLine("\nSo what'll it be? ");
+                userChoice = 0;
+                isValid = int.TryParse(Console.ReadLine(), out userChoice);
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\nOption selected was successful.");
+        }
         public static void Start()
         {
             bool gameStart = true;
             PlayIntroduction();
             GetHeroName();
+            ShowMainMenu();
         }
         #endregion
     }
